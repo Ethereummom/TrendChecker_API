9d2c76d602aae2b40e8acd7818edee02724094a7
Sanghee Sim <sanghee_ok@jlabsoft.com>
2024-02-27 11:04:03 +0900

    Feat : Add Body : Add sources for Issue Token for test

--- src/main/java/com/pingpong/jlab/pingpong/global/controller/GlobalController.java
+++ src/main/java/com/pingpong/jlab/pingpong/global/controller/GlobalController.java
@@ -1,15 +1,38 @@
 package com.pingpong.jlab.pingpong.global.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.pingpong.jlab.pingpong.domain.user.entity.User;
+import com.pingpong.jlab.pingpong.domain.user.repository.UserRepository;
+import com.pingpong.jlab.pingpong.global.api.ApiResponse;
+import com.pingpong.jlab.pingpong.global.dto.UserInfo;
+import com.pingpong.jlab.pingpong.global.error.exception.ErrorCode;
+import com.pingpong.jlab.pingpong.global.jwt.JwtAuthentication;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.Optional;
+
+@Tag(name = "Common Controller" , description = "공용 글로벌 API")
 @RestController
-@RequestMapping("/")
+@RequestMapping("/api/v1")
 public class GlobalController {
+
+    @Autowired
+    UserRepository userRepository;
 
     @RequestMapping("/login")
     public String userLogin(){
         return "hello";
+    }
+
+    @PostMapping("/oauth/token")
+    private ApiResponse getAuthentication(@RequestBody UserInfo userInfo){
+
+        Optional<User> user = userRepository.findByUserid(userInfo.getUserId());
+        if(user.isEmpty()){
+            return ApiResponse.res(400, ErrorCode.USER_NOT_FOUND.getMessage());
+        }
+        return null;
     }
 
 }
---
+++ src/main/java/com/pingpong/jlab/pingpong/global/dto/UserInfo.java
@@ -0,0 +1,15 @@
+package com.pingpong.jlab.pingpong.global.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+public class UserInfo {
+
+    private String userId;
+    private String passwd;
+
+}
